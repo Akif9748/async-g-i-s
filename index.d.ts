6d7bb3f5c5874da988d277950fbe18af5de82136
Akif9748 <akif9748@gmail.com>
2024-02-02 00:52:39 +0300

    Color support & old regex removed

--- index.d.ts
+++ index.d.ts
@@ -8,18 +8,16 @@ interface Result {
 interface Options {
     query?: object;
     userAgent?: string;
-    newRegex?: boolean;
 }
 
 /**
  * 
  * Async version of g-i-s module
  * @async
- * @param {String} searchTerm Search term for search
+ * @param {String} searchTerm Search term to search
  * @param {Options} options Options for search
- * @param {Object} options.query You can use custom query
+ * @param {Object} options.query You can use a custom query
  * @param {String} options.userAgent User agent for request
- * @param {Boolean} options.newRegex Better filtering, if you want to find everything, set as false
  * @returns {Promise<Result[]>} Array of results
  */
 export default function gis(searchTerm: string, options?: Options): Promise<Result[]>;
